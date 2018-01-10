f7342c8c3f990d2ed0ea177d1aff9ce36ffb09fd
Tomasz Górecki <tomasz.gorecki@ts.fujitsu.com>
2018-01-10 13:46:15 +0100

    Day 5, part 1

---
+++ Day5/nice-words.hs
@@ -0,0 +1,24 @@
+import Data.List
+
+isStringNice :: String -> Bool
+isStringNice string = has3Vovels string
+                   && hasDuplicateLetter string
+                   && doesntHaveForbidden string
+
+has3Vovels :: String -> Bool
+has3Vovels string = length vovels >= 3
+    where vovels = filter (\x -> x `elem` "aeiou") string
+
+hasDuplicateLetter :: String -> Bool
+hasDuplicateLetter string = any (\substr -> length substr >= 2) $ group string
+
+doesntHaveForbidden :: String -> Bool
+doesntHaveForbidden string = not $ any (\forbidden -> forbidden `isInfixOf` string) forbiddens
+    where forbiddens = ["ab", "cd", "pq", "xy"]
+
+main = do
+    contents <- readFile "input"
+
+    let niceStrings = filter isStringNice $ lines contents
+
+    print $ length niceStrings
