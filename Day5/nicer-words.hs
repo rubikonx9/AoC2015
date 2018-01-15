8fcd875128a271de748fcf7b2cd0cbdb168dd3ee
Tomasz Górecki <tomasz.gorecki@ts.fujitsu.com>
2018-01-15 11:55:36 +0100

    Day 5, part 2

---
+++ Day5/nicer-words.hs
@@ -0,0 +1,34 @@
+import Data.List
+
+isStringNice :: String -> Bool
+isStringNice string = hasNiceDuplicate string
+                   && hasPair string
+
+isLength3 :: String -> Bool
+isLength3 string = length string == 3
+
+isNiceDuplicate :: String -> Bool
+isNiceDuplicate string = string !! 0 == string !! 2
+
+hasNiceDuplicate :: String -> Bool
+hasNiceDuplicate string = any isNiceDuplicate realSubseqs3
+    where realSubseqs3 = filter (\x -> x `isInfixOf` string) subseqs3
+          subseqs3     = filter isLength3 subseqs
+          subseqs      = subsequences string
+
+main = do
+    contents <- readFile "input"
+
+    let niceStrings = filter isStringNice $ lines contents
+    print $ length niceStrings
+
+hasPair :: String -> Bool
+hasPair string = any (== True) checks
+    where checks = [ word2 `elem` subsequentSubstrings | x <- [0..length substrings - 1],
+              let subsequentSubstrings = drop (x + 2) substrings,
+              let word2 = (take 1 $ drop x substrings) !! 0 ]
+          substrings = len2Substrings string
+
+len2Substrings :: String -> [String]
+len2Substrings string = [ [ string !! x, string !! (x + 1) ] | x <- [0..length string - 2] ] 
+    
