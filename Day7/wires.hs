b88d8a8a1203819ace791316396d9f9f68e9b369
Tomasz Górecki <tomasz.gorecki@ts.fujitsu.com>
2018-01-29 10:01:14 +0100

    Day 7, part 1

---
+++ Day7/wires.hs
@@ -0,0 +1,99 @@
+import Data.Bits
+import Data.Maybe
+import Data.List
+import Data.List.Split
+import Data.Word
+import Text.Read
+
+data Operation = NOP | ASSIGN | AND | OR | LSHIFT | RSHIFT | NOT
+    deriving (Show, Eq)
+
+data Instruction = Instruction {
+    operation   :: Operation,
+    outputName  :: String,
+    inputsNames :: [String]
+} deriving (Show)
+
+parseInstruction :: String -> Instruction
+parseInstruction rawInstruction = Instruction { operation = operation, inputsNames = inputsNames, outputName = outputName }
+    where inputParts               = splitOn " " inputs
+          (inputs, outputName)     = parse rawInstruction
+          (operation, inputsNames) = if length inputParts == 1        then (ASSIGN, [inputParts !! 0])
+                                else if (inputParts !! 0) == "NOT"    then (NOT,    [inputParts !! 1])
+                                else if (inputParts !! 1) == "AND"    then (AND,    [inputParts !! 0, inputParts !! 2])
+                                else if (inputParts !! 1) == "OR"     then (OR,     [inputParts !! 0, inputParts !! 2])
+                                else if (inputParts !! 1) == "LSHIFT" then (LSHIFT, [inputParts !! 0, inputParts !! 2])
+                                else if (inputParts !! 1) == "RSHIFT" then (RSHIFT, [inputParts !! 0, inputParts !! 2])
+                                else (NOP, [])
+
+getInstruction :: String -> [Instruction] -> Instruction
+getInstruction instructionName allInstructions = fromMaybe dum $ find (\x -> outputName x == instructionName) allInstructions
+    where dum = Instruction {  operation = NOP, inputsNames = [], outputName = ""  }
+
+execByNames :: [String] -> [Instruction] -> [Word16]
+execByNames instructionsNames allInstructions = map (\x -> execByName x allInstructions) instructionsNames
+
+execByName :: String -> [Instruction] -> Word16
+execByName instructionName allInstructions = output
+    where output            = exec sourceInstruction allInstructions
+          sourceInstruction = getInstruction (instructionName) allInstructions
+
+exec :: Instruction -> [Instruction] -> Word16
+exec instruction allInstructions = result
+    where op     = operation instruction
+          result = if op == ASSIGN then execAssign instruction allInstructions
+              else if op == NOT    then execNot    instruction allInstructions
+              else if op == AND    then execAnd    instruction allInstructions
+              else if op == OR     then execOr     instruction allInstructions
+              else if op == LSHIFT then execLshift instruction allInstructions
+              else if op == RSHIFT then execRshift instruction allInstructions
+              else 0
+
+execAssign :: Instruction -> [Instruction] -> Word16
+execAssign instruction allInstructions = fromMaybe source maybeResult
+    where maybeResult       = readMaybe inputName
+          inputName         = inputsNames instruction !! 0
+          source            = execByName inputName allInstructions
+
+execNot :: Instruction -> [Instruction] -> Word16
+execNot instruction allInstructions = complement (sources !! 0)
+    where sources = execByNames (inputsNames instruction) allInstructions
+
+execAnd :: Instruction -> [Instruction] -> Word16
+execAnd instruction allInstructions = (fromMaybe sourceA maybeNumber) .&. sourceB
+    where maybeNumber = readMaybe inputNameA
+          inputNameA  = inputsNames instruction !! 0
+          inputNameB  = inputsNames instruction !! 1
+          sourceA     = execByName inputNameA allInstructions
+          sourceB     = execByName inputNameB allInstructions
+
+execOr :: Instruction -> [Instruction] -> Word16
+execOr instruction allInstructions = (sources !! 0) .|. (sources !! 1)
+    where sources = execByNames (inputsNames instruction) allInstructions
+
+execLshift :: Instruction -> [Instruction] -> Word16
+execLshift instruction allInstructions = source `shiftL` offset
+    where source = execByName ((inputsNames instruction) !! 0) allInstructions
+          offset = read ((inputsNames instruction) !! 1)
+
+execRshift :: Instruction -> [Instruction] -> Word16
+execRshift instruction allInstructions = source `shiftR` offset
+    where source = execByName ((inputsNames instruction) !! 0) allInstructions
+          offset = read ((inputsNames instruction) !! 1)
+
+parse :: String -> (String, String)
+parse str = (input, output)
+    where parts  = splitOn " -> " str
+          output = parts !! 1
+          input  = parts !! 0
+
+notEmpty :: String -> Bool
+notEmpty str = not $ null str
+
+main = do
+    contents <- readFile "input"
+
+    let rawInstructions = filter notEmpty (lines contents)
+    let allInstructions = map parseInstruction rawInstructions
+
+    print $ execByName "a" allInstructions
---
+++ Day7/wires.js
@@ -0,0 +1,178 @@
+#!/usr/bin/env node
+
+const FS      = require("fs");
+const Signage = require("signage");
+const uint16  = Signage.short.unsigned;
+
+const Cache = {};
+
+class Instruction {
+    constructor(outputName) {
+        this.outputName = outputName;
+    }
+
+    cacheOrExec(allInstructions) {
+        if (this.outputName && this.outputName in Cache) {
+            return Cache[this.outputName];
+        }
+
+        const result = this.exec(allInstructions);
+
+        Cache[this.outputName] = result;
+
+        return result;
+    }
+
+    static fromText(text) {
+        const [input, outputName] = text.split(" -> ");
+        let matches;
+        let instruction;
+
+        if (input.match(/^(\d+|\S+)$/)) {
+            instruction = new Assign(outputName, input);
+        } else if (matches = input.match(/^NOT\s+(\S+)/)) {
+            instruction = new Not(outputName, matches[1]);
+        } else if (matches = input.match(/^(\S+)\s+AND\s+(\S+)/)) {
+            instruction = new And(outputName, matches[1], matches[2]);
+        } else if (matches = input.match(/^(\S+)\s+OR\s+(\S+)/)) {
+            instruction = new Or(outputName, matches[1], matches[2]);
+        } else if (matches = input.match(/^(\S+)\s+LSHIFT\s+(\S+)/)) {
+            instruction = new LShift(outputName, matches[1], matches[2]);
+        } else if (matches = input.match(/^(\S+)\s+RSHIFT\s+(\S+)/)) {
+            instruction = new RShift(outputName, matches[1], matches[2]);
+        } else {
+            instruction = new NOP(outputName);
+        }
+
+        return instruction;
+    }
+}
+
+class NOP extends Instruction {
+    exec() {
+        throw new Error("Not implemented");
+    }
+}
+
+class RawNumber extends Instruction {
+    constructor(number) {
+        super();
+        this.number = number;
+    }
+
+    exec() {
+        return uint16(this.number);
+    }
+}
+
+class Assign extends Instruction {
+    constructor(outputName, instructionName) {
+        super(outputName);
+        this.instructionName = instructionName;
+    }
+
+    exec(allInstructions) {
+        return allInstructions.exec(this.instructionName);
+    }
+}
+
+class Not extends Instruction {
+    constructor(outputName, instructionName) {
+        super(outputName);
+        this.instructionName = instructionName;
+    }
+
+    exec(allInstructions) {
+        return uint16(~(allInstructions.exec(this.instructionName)));
+    }
+}
+
+class And extends Instruction {
+    constructor(outputName, instructionNameA, instructionNameB) {
+        super(outputName);
+        this.instructionNameA = instructionNameA;
+        this.instructionNameB = instructionNameB;
+    }
+
+    exec(allInstructions) {
+        return uint16(allInstructions.exec(this.instructionNameA) & allInstructions.exec(this.instructionNameB));
+    }
+}
+
+class Or extends Instruction {
+    constructor(outputName, instructionNameA, instructionNameB) {
+        super(outputName);
+        this.instructionNameA = instructionNameA;
+        this.instructionNameB = instructionNameB;
+    }
+
+    exec(allInstructions) {
+        return uint16(allInstructions.exec(this.instructionNameA) | allInstructions.exec(this.instructionNameB));
+    }
+}
+
+class LShift extends Instruction {
+    constructor(outputName, instructionNameA, instructionNameB) {
+        super(outputName);
+        this.instructionNameA = instructionNameA;
+        this.instructionNameB = instructionNameB;
+    }
+
+    exec(allInstructions) {
+        return uint16(allInstructions.exec(this.instructionNameA) << allInstructions.exec(this.instructionNameB));
+    }
+}
+
+class RShift extends Instruction {
+    constructor(outputName, instructionNameA, instructionNameB) {
+        super(outputName);
+        this.instructionNameA = instructionNameA;
+        this.instructionNameB = instructionNameB;
+    }
+
+    exec(allInstructions) {
+        return uint16(allInstructions.exec(this.instructionNameA) >> allInstructions.exec(this.instructionNameB));
+    }
+}
+
+class Instructions extends Array {
+    get(name) {
+        const instruction = this.find(instruction => {
+            return instruction.outputName === name;
+        });
+
+        if (instruction) {
+            return instruction;
+        }
+
+        const matches = name.match(/^(\d+)$/);
+
+        if (matches) {
+            return new RawNumber(parseInt(matches[1]));
+        }
+
+        throw new Error(`No entry for '${name}' found`);
+    }
+
+    exec(name) {
+        return this.get(name).cacheOrExec(this);
+    }
+}
+
+FS.readFile("input", "utf8", function(error, contents) {
+    if (error) {
+        return;
+    }
+
+    const lines = contents.split("\n").filter(line => {
+        return line !== null;
+    });
+
+    const instructions = Instructions.from(lines.map(line => {
+        return Instruction.fromText(line);
+    }));
+
+    const instructionForA = instructions.get("a");
+
+    console.log(instructionForA.exec(instructions));
+});
